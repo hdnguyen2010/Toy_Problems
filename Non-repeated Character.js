86ced0baeeeb33416196eb66077f35b7f99a5a6f
Huy Nguyen <hdnguyen2010@gmail.com>
2016-07-02 19:50:59 -0500

    non-repeated Character completed

--- Non-repeated Character.js
+++ Non-repeated Character.js
@@ -17,7 +17,7 @@ string:
 function firstNonRepeatedCharacter (string) {
 	for(var i = 0; i < string.length; i++) {
 		var char = string.charAt(i);
-		if(string.indexOf(char) === i && string.indexOf(char, i + 1) === -1){
+		if(string.indexOf(char) === i && string.indexOf(char, i + 1) === -1) {
 			return char;
 		}
 	}
